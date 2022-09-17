a630dff3b7307848ed3978ba0fad9cb46a527ea2
Mathias <mathias.yde@hotmail.dk>
2022-09-18 01:27:40 +0200

    refactored HandUpdater script to allow for all SteamVR sources

--- Assets/Scripts/HandUpdater.cs
+++ Assets/Scripts/HandUpdater.cs
@@ -6,55 +6,85 @@ using Valve.VR;
 using Valve.VR.InteractionSystem;
 
 public class HandUpdater : MonoBehaviour {
-    [SerializeField] private Hand leftHand;
-    [SerializeField] private Hand rightHand;
     [SerializeField] private Transform head;
 
-    private HandPoseData currentLeftHand;
-    private HandPoseData currentRightHand;
+    [SerializeField]
+    private GenericDictionary<SteamVR_Input_Sources, Color> sourceColorMap =
+        new GenericDictionary<SteamVR_Input_Sources, Color>();
+    
+    [SerializeField] private GenericDictionary<SteamVR_Input_Sources, Hand> sources =
+        new GenericDictionary<SteamVR_Input_Sources, Hand>();
 
+#if UNITY_EDITOR
+    private GenericDictionary<SteamVR_Input_Sources, HandPoseData> handData = new GenericDictionary<SteamVR_Input_Sources, HandPoseData>();
+#endif
+    
+    
     private void Start() {
-        if (!leftHand) { Debug.LogError("Left hand not initialized", this); }
-        if (!rightHand) { Debug.LogError("Right hand not initialized", this); }
+        // log warning if some hands are not initialized in sources dictionary
+        foreach ((SteamVR_Input_Sources source, Hand hand) in sources) {
+            if (hand == null) {
+                Debug.LogWarning($"{hand} not initialized, from source: {source}", this);
+            }
+        }
+        
+        HandTracking.Start();
     }
-
-    private HandPoseData PopulateHandPoseData(Hand hand, HandPoseData handData) {
-        handData.thumbCurl = hand.skeleton.thumbCurl;
-        handData.indexCurl = hand.skeleton.indexCurl;
-        handData.middleCurl = hand.skeleton.middleCurl;
-        handData.ringCurl = hand.skeleton.ringCurl;
-        handData.pinkyCurl = hand.skeleton.pinkyCurl;
-
-        handData.head = head.position;
-
-        // TODO(mathias) make relative to camera forward direction
-        handData.offset.x = (head.position.x - hand.skeleton.GetBone(1).position.x) * -1;
-        handData.offset.y = (head.position.y - hand.skeleton.GetBone(1).position.y) * -1;
-        handData.offset.z = (head.position.z - hand.skeleton.GetBone(1).position.z) * -1;
- 
-        return handData;
-    }
-
+    
     private void Update() {
-        if (rightHand.skeleton) {
-            HandPoseData right = new HandPoseData();
-            right = PopulateHandPoseData(rightHand, right);
-            HandTracking.UpdateHand(SteamVR_Input_Sources.RightHand, right);
-            currentRightHand = right;
+        // update HandTracking about each hand
+        foreach ((SteamVR_Input_Sources source, Hand hand) in sources) {
+            HandPoseData handPoseData = new HandPoseData();
+            PopulateHandPoseData(source, handPoseData, hand);
+            HandTracking.UpdateHand(source, handPoseData);
         }
+        
+        HandTracking.Update();
+    }
 
-        if (leftHand.skeleton) {
-            HandPoseData left = new HandPoseData();
-            left = PopulateHandPoseData(leftHand, left);
-            HandTracking.UpdateHand(SteamVR_Input_Sources.LeftHand, left);
-            currentLeftHand = left;
+    // Populate HandPoseData struct for a hand
+    private void PopulateHandPoseData_Hand(HandPoseData handPoseData, Hand hand) {
+        handPoseData.thumbCurl = hand.skeleton.thumbCurl;
+        handPoseData.indexCurl = hand.skeleton.indexCurl;
+        handPoseData.middleCurl = hand.skeleton.middleCurl;
+        handPoseData.ringCurl = hand.skeleton.ringCurl;
+        handPoseData.pinkyCurl = hand.skeleton.pinkyCurl;
+        
+        handPoseData.head = head.position;
+        
+        // TODO(mathias) make relative to camera forward direction
+        Transform wrist = hand.skeleton.GetBone(1);
+        
+        handPoseData.offset = (head.position - wrist.position) * -1;
+        
+        // handPoseData.offset.x = (head.position.x - wrist.position.x) * -1;
+        // handPoseData.offset.y = (head.position.y - wrist.position.y) * -1;
+        // handPoseData.offset.z = (head.position.z - wrist.position.z) * -1;
+    }
+    
+    private void PopulateHandPoseData(SteamVR_Input_Sources source, HandPoseData handPoseData, Hand hand) {
+        handPoseData.source = source;
+        
+        switch (source) {
+            case SteamVR_Input_Sources.RightHand:
+                PopulateHandPoseData_Hand(handPoseData, hand);
+                break;
+            case SteamVR_Input_Sources.LeftHand:
+                PopulateHandPoseData_Hand(handPoseData, hand);
+                break;
+            default:
+                Debug.LogWarning("Tried to populate hand pose data for non-existing source");
+                break;
         }
     }
 
+#if UNITY_EDITOR
     private void OnDrawGizmos() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(head.position, currentLeftHand.offset);
-        Gizmos.color = Color.blue;
-        Gizmos.DrawRay(head.position, currentRightHand.offset);
+        // draw lines between head position to each hand
+        foreach ((SteamVR_Input_Sources source, HandPoseData hand) in handData) {
+            Gizmos.color = sourceColorMap.Get(source, Color.black);
+            Gizmos.DrawRay(head.position, hand.offset);
+        }
     }
+#endif
 }
