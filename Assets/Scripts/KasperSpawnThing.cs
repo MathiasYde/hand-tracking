a3eeebc8791de58f82391bde032f0ce83439c8a2
mathias <mathiasydehtx@gmail.com>
2022-10-04 13:23:19 +0200

    got the map to work

---
+++ Assets/Scripts/KasperSpawnThing.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KasperSpawnThing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Now spawning a thing in start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Now spawning a thing in update");
+    }
+
+    void ping()
+    {
+        Debug.Log("Now spawning a thing");
+    }
+
+}
